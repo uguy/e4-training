ae94299e060933294a04a147c83d3254b4afd6ca
hugues.bretin <hugues.bretin@bonitasoft.com>
2022-03-24 14:32:18 +0100

    rcp

---
+++ com.bonitasoft.rental.rcp/src/com/bonitasoft/rental/rcp/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,20 @@
+package com.bonitasoft.rental.rcp;
+
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+import com.bonitasoft.rental.ui.RentalUIConstants;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+	@Override
+	public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
+		return new ApplicationWorkbenchWindowAdvisor(configurer);
+	}
+
+	@Override
+	public String getInitialWindowPerspectiveId() {
+		return RentalUIConstants.RENTAL_UI_PERSPECTIVE;
+	}
+}
